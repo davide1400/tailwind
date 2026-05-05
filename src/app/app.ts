82c668ea0810590fce6c51bcb73f48932888ccfb
davide <xxxxxx+tuoutente@users.noreply.github.com>
2026-05-05 15:58:48 +0200

    implements gradient + cambio tema

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,25 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('tailwind-app');
+
+  constructor(){
+    const theme = localStorage.getItem('theme') ?? 'light';
+    document.documentElement.setAttribute('data-theme', theme);
+  }
+
+  switchTheme(){
+    const html = document.documentElement;
+    const currentTheme = html.getAttribute('data-theme');
+
+    if(currentTheme === 'dark'){
+      html.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
+    }else{
+      html.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+    }
+
+
+  }
+
 }
