07764d7c4b0ffc5d5d12f28e27605822610bec6a
davide <xxxxxx+tuoutente@users.noreply.github.com>
2026-05-10 17:27:43 +0200

    si deve fixare l'immagine

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "./pages/header/header";
 import { Footer } from "./pages/footer/footer";
+import { Header } from "./pages/header/header";
+import { AstaLive } from './pages/core/asta-live/asta-live';
+import { Cards } from "./pages/core/asta-live/cards/cards";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, AstaLive, Cards],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
